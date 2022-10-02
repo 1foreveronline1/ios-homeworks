736ec79b879cb32cd2946e57b2575e2009dbe9a7
1foreveronline1 <andrey29082002@gmail.com>
2022-10-02 16:07:04 +0300

    Выполнены задания 4-6

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Андрей Аснач on 02.10.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    @IBOutlet weak var profilePicture: UIImageView!
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var profileDate: UILabel!
+    @IBOutlet weak var profilelocation: UILabel!
+    @IBOutlet weak var textField: UITextView!
+}
